f45d373667b6a6a806a6f72b689402fbf73592b3
Mark Wright <sparkymeister@gmail.com>
2018-07-13 18:37:47 -0700

    upgrade ubuntu and php

--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y php7.0-cli php7.0-mbstring php7.0-xml php7.0-zip git zip
+apt-get install -y php7.2-cli php7.2-mbstring php7.2-xml php7.2-zip git zip
 
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
@@ -13,7 +13,7 @@ composer install
 
 apt-get install php-xdebug
 
-cat > /etc/php/7.0/cli/conf.d/30-xdebug_settings.ini << EOF
+cat > /etc/php/7.2/cli/conf.d/30-xdebug_settings.ini << EOF
 xdebug.remote_enable=1
 xdebug.remote_handler=dbgp
 xdebug.remote_connect_back=1
@@ -21,3 +21,5 @@ error_reporting=E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED
 EOF
 
 ln -s /vagrant /home/vagrant
+sudo chgrp vagrant ubuntu
+sudo chmod 775 ubuntu
