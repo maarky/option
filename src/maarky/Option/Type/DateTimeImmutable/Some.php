e068c3530616a2e3ff831c06e9e212c1021d1acb
Mark Wright <sparkymeister@gmail.com>
2018-07-18 22:16:23 -0700

    added DateTime and DateTimeImmutable

---
+++ src/maarky/Option/Type/DateTimeImmutable/Some.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace maarky\Option\Type\DateTimeImmutable;
+
+use maarky\Option\Type\Object\Option as ObjectOption;
+use maarky\Option\Component\BaseSome;
+
+class Some extends Option
+{
+    use BaseSome;
+}
